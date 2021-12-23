f8b4fabae8db849f07a48e85b0543525e99ad8cb
Bash <bashirmohammed@38-f9-d3-d0-d8-3c.dhcp.lbnl.us>
2021-12-23 12:23:03 -0800

    UPDATE

--- local_deploy-u/app.py
+++ local_deploy-u/app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from datetime import datetime, timedelta
+
 import pandas as pd
 import numpy as np
-import joblib 
+
+from statsmodels.tsa.vector_ar.vecm import coint_johansen
 from statsmodels.tsa.vector_ar.var_model import VAR
 
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+from use_functions import ( forecast_plot, rmse_results, fit_predict_lstm,
+                           fit_predict_var )
 
 import warnings
 warnings.simplefilter(action='ignore')
@@ -14,84 +15,37 @@ warnings.simplefilter(action='ignore')
 
 def main():
     st.set_page_config(layout = "wide")
-    page = st.sidebar.selectbox('Select page', ['NetGraf Data','NetGraf ML Plot'])
-    st.title('')
-    
-    df = pd.read_csv('datasets/throughput_metrics.csv', parse_dates=['Time'], index_col='Time')
+    page = st.sidebar.selectbox('Select Algorithm:', ['VAR','LSTM'])
+    st.title('NetGraf ML Predict Dashboard')
+
+    # load dataframe
+    df = pd.read_csv('datasets/throughput.csv', parse_dates=['Time'], index_col='Time')
+    df_print = pd.read_csv('datasets/throughput.csv', parse_dates=['Time'])
+
+    st.markdown('#### Dataset:')
+    st.write(df_print)
+    st.write(f'Dataset shape: {df.shape}')
+
+    st.markdown('#### NetGraf Dataset Statistics:')
+    st.write(df.describe().T)
+
     # missing value treatment
-    df['SiteB'][df['SiteB'] == 0] = df['SiteB'].mean()
-    
-    # fit model and forecast
-    model = VAR(df)  
-    model_fit = model.fit() 
-    yhat = model_fit.forecast(model_fit.y, steps=150)
-    
-    def forecast_to_df(data, forecast):
-        ''' Convert forecast output array to dataframe using input data columns
-        and input data last date '''
-        date = data[-1:].index 
-        future_date = []
-        for i in range(len(forecast)): 
-            date += timedelta(hours=1)
-            future_date.append(date)
-        yhat_df = pd.DataFrame(yhat, columns=data.columns, index=list(map(list, zip(*future_date))))
-        # convert index values to datetime 
-        idx = yhat_df.index.get_level_values(0).astype(str) 
-        yhat_df.index = pd.to_datetime(idx)
-        yhat_df.index.rename('Time', inplace=True)
-        
-        return yhat_df 
-    
-    yhat_df = forecast_to_df(df, yhat)
-    
-    # make forecast data similar to input data
-    forecast_df = yhat_df.copy()
-    forecast_df.reset_index(level=0, inplace=True)
+    for i in ['Node 138.68.173.232', 'Node 192.5.86.169']:
+        df[i][df[i] == 0] = df[i].mean()
 
-    # st.markdown("## Forecast Data")
-    # st.write(forecast_df) 
-    
-    # st.markdown("## Forecast Plot")
-    fig = make_subplots(rows=6, cols=1, subplot_titles=df.columns,
-                    vertical_spacing=0.05)
+    st.markdown("#### NetGraf Test vs Prediction Plot:")
 
-    for i, col in enumerate(df.columns):
-        fig.add_trace(go.Scatter(x=df.index, y=df[col],  
-                                marker = dict(size = 10, 
-                                            color = 'blue'),
-                                textfont=dict(
-                                    color='black',
-                                    size=18,  
-                                    family='Times New Roman')),
-                    row=i+1, col=1)
-        fig.add_trace(go.Scatter(x=yhat_df.index, y=yhat_df[col],  
-                                marker = dict(size = 10, 
-                                            color = 'red')),
-                    row=i+1, col=1)
-        
-    fig.update_xaxes(
-        rangeselector=dict(
-            buttons=list([
-                dict(count=1, label="1m", step="month", stepmode="backward"),
-                dict(count=6, label="6m", step="month", stepmode="backward"),
-                dict(count=1, label="YTD", step="year", stepmode="todate"),
-                dict(count=1, label="1y", step="year", stepmode="backward"),
-                dict(step="all")
-            ])
-        )
-    )
-    fig.update_layout(showlegend=False, autosize=False,
-        width=1300,
-        height=1500,
-        )
-    
-    if page == 'NetGraf Data':
-        st.markdown("## NetGraf Data")
-        st.write(forecast_df) 
+
+    if page == 'VAR':
+        df_train, df_test, fc = fit_predict_var(df)
+        fig = forecast_plot(df_train, df_test, fc, ['Train', 'Test', 'Predict'], 'VAR')
+        st.plotly_chart(fig)
+
     else:
-        st.markdown("## NetGraf ML Plot")
-        st.plotly_chart(fig) 
-    
-    
+        n_train_hours, inv_y, inv_yhat = fit_predict_lstm(df)
+        df_train, df_test = df.iloc[:n_train_hours, :], df.iloc[n_train_hours:, :]
+        fig = forecast_plot(df_train, df_test, inv_yhat, ['Train', 'Test', 'Predict'], 'LSTM')
+        st.plotly_chart(fig)
+
 if __name__ == "__main__":
-    main() 
+    main()
